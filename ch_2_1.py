446287c4447daaa86a4958febe8d17a0aa7df35c
lione78 <lione78@nate.com>
2023-06-19 18:30:56 +0900

    first commit

---
+++ ch_2_1.py
@@ -0,0 +1,7 @@
+# chapter 2 1번째 연습문제
+
+number = {'국어' : 80, '영어' : 75, '수학' : 55}
+sum = number.get('국어') + number.get('영어') + number.get('수학')
+avg = sum/len(number)
+
+print(avg)
---
+++ ch_2_2.py
@@ -0,0 +1,8 @@
+# chapter 2 2번째 연습문제
+
+a = 13
+
+if a % 2 == 0:
+    print("a는 짝수입니다.")
+else:
+    print("a는 홀수입니다.")
